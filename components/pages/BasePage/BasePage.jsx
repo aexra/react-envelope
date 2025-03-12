652c9e9430c4d85182e4e8811cb9ded3451dd34a
Nikita Fomin <defdXs@yandex.ru>
2025-03-13 01:26:02 +0300

    Some icons

--- components/pages/BasePage/BasePage.jsx
+++ components/pages/BasePage/BasePage.jsx
@@ -3,7 +3,7 @@ import VBoxPanel from '../../layouts/VBoxPanel/VBoxPanel';
 import css from './BasePage.module.css';
 import NavSidebar from '../../widgets/NavSidebar/NavSidebar';
 
-function BasePage({ ref, className, style, children, hasHeader = true, bodyGap = '40px', hasNavSidebar = true, headerContent, footerContent }) {
+function BasePage({ ref, className, style, children, hasHeader = true, bodyGap = '40px', bodyClassName, hasNavSidebar = true, headerContent, footerContent }) {
     return (
         <VBoxPanel  className={`${className} ${css.page} ${!footerContent && css.footerless} ${hasNavSidebar && css.pageSidebarMargin}`}
                     style={{
@@ -13,7 +13,7 @@ function BasePage({ ref, className, style, children, hasHeader = true, bodyGap =
                     gap='40px'>
             {hasNavSidebar && <NavSidebar/>}
             {hasHeader && <Header>{headerContent}</Header>}
-            <VBoxPanel className={`${css.body}`}
+            <VBoxPanel className={`${css.body} ${bodyClassName}`}
                        gap={bodyGap}>
                 {children}
             </VBoxPanel>
--- components/pages/SamplesPage/SamplesPage.jsx
+++ components/pages/SamplesPage/SamplesPage.jsx
@@ -1,6 +1,9 @@
 import css from './SamplesPage.module.css';
 import BasePage from '../BasePage/BasePage';
 import DSTUFooter from '../../widgets/DSTUFooter/DSTUFooter';
+import { Close, Code, Email, Home, Logout, Moon, Sun, Swap, User, UserAdd } from '../../dummies/Icons';
+import FlowPanel from '../../layouts/FlowPanel/FlowPanel';
+import HDivider from '../../ui/dividers/HDivider/HDivider';
 
 export const SamplesPage = () => {
     const setHeaderContent = () => {
@@ -11,6 +14,22 @@ export const SamplesPage = () => {
 
     return (
         <BasePage headerContent={setHeaderContent()}
-                  footerContent={<DSTUFooter/>}/>
+                  footerContent={<DSTUFooter/>}
+                  bodyClassName={`fullh`}>
+            <h1>Иконки</h1>
+            <FlowPanel className={``}>
+                <Moon/>
+                <Sun/>
+                <User/>
+                <UserAdd/>
+                <Home/>
+                <Code/>
+                <Swap/>
+                <Close/>
+                <Logout/>
+                <Email/>
+            </FlowPanel>
+            <HDivider className={`stretch-self`}/>
+        </BasePage>
     );
 };
