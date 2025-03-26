de767da9fe9a2802677f6012c2c15a96c985d29a
Nikita Fomin <defdXs@yandex.ru>
2025-03-26 17:26:11 +0300

    Fixed footer

--- components/widgets/DSTUFooter/DSTUFooter.jsx
+++ components/widgets/DSTUFooter/DSTUFooter.jsx
@@ -7,7 +7,7 @@ import rArrow from '../../../assets/vectors/rArrow-w.svg';
 function DSTUFooter({ ref, className, style }) {
     return (
         <HBoxPanel ref={ref}
-                   className={`${css.footer} ${className}`}
+                   className={`${css.footer} ${className} stretch-self`}
                    style={style}
                    gap='20px'
                    valign='center'>
