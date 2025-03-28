847043d211adf2639491a0d79100e50bcf2988c5
Nikita Fomin <defdXs@yandex.ru>
2025-03-29 01:16:15 +0300

    lets start new authcard

--- components/pages/DevExpPage/DevExpPage.jsx
+++ components/pages/DevExpPage/DevExpPage.jsx
@@ -1,14 +1,15 @@
 import { Code } from '../../dummies/Icons';
 import { HeaderTitle } from '../../dummies/styleless/HeaderTitle';
 import DSTUNavSidebar from '../../widgets/DSTUNavSidebar/DSTUNavSidebar';
+import { AuthCard } from '../../widgets/user/AuthCard/AuthCard';
+import { PageBase } from '../base/PageBase/PageBase';
 import BasePage from '../BasePage/BasePage';
 import css from './DevExpPage.module.css';
 
 export const DevExpPage = () => {
     return (
-        <BasePage navSidebar={<DSTUNavSidebar/>}
-                  headerContent={<HeaderTitle text='Экспериментальная' icon={<Code/>}/>}>
-
-        </BasePage>
+        <PageBase>
+            <AuthCard/>
+        </PageBase>
     );
 };
---
+++ components/widgets/user/AuthCard/AuthCard.jsx
@@ -0,0 +1,10 @@
+import HBoxPanel from '../../../layouts/HBoxPanel/HBoxPanel';
+import css from './AuthCard.module.css';
+
+export const AuthCard = () => {
+    return (
+        <HBoxPanel className={`${css.card}`}>
+
+        </HBoxPanel>
+    );
+};
