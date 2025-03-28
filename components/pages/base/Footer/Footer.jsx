58dfadae04d6ade01c80778d87d52a791034172d
Никита Фомин <defdXs@yandex.ru>
2025-03-28 17:07:06 +0300

    default footer done

--- components/pages/base/Footer/Footer.jsx
+++ components/pages/base/Footer/Footer.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import css from './Footer.module.css';
+import { useNavigation } from '../../../../hooks/useNavigation';
+import { NavLink } from 'react-router-dom';
 
-export const Footer = () => {
+const FooterNavLink = ({
+    name,
+    to
+}) => {
+    return (
+        <NavLink to={to} className={({ isActive, isPending }) => `
+            ${isPending ? '' : isActive ? css.active : ''} ${css.navlink}
+            `}>{name}</NavLink>
+    );
+};
+
+export const Footer = ({
+    routes,
+    navLink = FooterNavLink
+}) => {
+    const [links, setLinks] = useState([]);
+    const { navlinks } = useNavigation();
+
+    useEffect(() => {
+        if (routes) {
+            setLinks(navlinks(navLink, routes));
+        }
+    }, []);
+
     return (
         <div className={`flex col ${css.foo}`}>
-            <div className={`flex row ${css.links}`}>
-
+            <div className={`flex row center g20 ${css.links}`}>
+                {links}
             </div>
-            <span>Powered by <a>aexra</a></span>
+            <span className={`${css.powered} h-last v-last`}>Powered by <a target='blank' href='https://github.com/aexra'>aexra</a></span>
         </div>
     );
 };
