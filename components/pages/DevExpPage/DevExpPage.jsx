22d1f283bf9892bac7362733632428f0d2e941b3
Nikita Fomin <defdXs@yandex.ru>
2025-03-29 13:01:09 +0300

    Pivot done; several base fixes

--- components/pages/DevExpPage/DevExpPage.jsx
+++ components/pages/DevExpPage/DevExpPage.jsx
@@ -3,6 +3,7 @@ import { HeaderTitle } from '../../dummies/styleless/HeaderTitle';
 import { Switch } from '../../ui/selectors/Switch/Switch';
 import DSTUNavSidebar from '../../widgets/DSTUNavSidebar/DSTUNavSidebar';
 import { AuthCard } from '../../widgets/user/AuthCard/AuthCard';
+import { Pivot } from '../../wrappers/Pivot/Pivot';
 import { PageBase } from '../base/PageBase/PageBase';
 import BasePage from '../BasePage/BasePage';
 import css from './DevExpPage.module.css';
@@ -11,6 +12,17 @@ export const DevExpPage = () => {
     return (
         <PageBase>
             <AuthCard/>
+
+            <Pivot>
+                <Pivot.Keys>
+                    <span>Марина</span>
+                    <span>Aboba</span>
+                </Pivot.Keys>
+                <Pivot.Items>
+                    <span>Антонова</span>
+                    <span>Bebebe</span>
+                </Pivot.Items>
+            </Pivot>
         </PageBase>
     );
 };
---
+++ components/wrappers/Pivot/Pivot.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import css from './Pivot.module.css';
+
+export const Pivot = ({
+    className,
+    children
+}) => {
+    const [selected, select] = useState(0);
+
+    const keysElement = React.Children.toArray(children).find(
+        child => child.type === Pivot.Keys
+    );
+    const itemsElement = React.Children.toArray(children).find(
+        child => child.type === Pivot.Items
+    );
+
+    const keys = keysElement?.props.children || [];
+    const items = itemsElement?.props.children || [];
+
+    return (
+        <div className={`${css.pivot} flex col g5`}>
+            <div className={`${css.keys} flex row g5`}>
+                {React.Children.map(keys, (key, index) => (
+                    <button 
+                        key={index}
+                        className={`${css.key} textbutton pointer ${index == selected && css.selected}`}
+                        onClick={() => select(index)}
+                    >
+                        {key}
+                    </button>
+                ))}
+            </div>
+            <div className={`${css.content}`}>
+                {items[selected]}
+            </div>
+        </div>
+    );
+};
+
+Pivot.Keys = ({ children }) => <>{children}</>;
+Pivot.Items = ({ children }) => <>{children}</>;
