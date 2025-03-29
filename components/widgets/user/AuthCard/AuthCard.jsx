9ae7e7fd4e47ba735dcdbce48bbc805122c4da83
Nikita Fomin <defdXs@yandex.ru>
2025-03-29 14:16:06 +0300

    Several styles fixes

--- components/widgets/user/AuthCard/AuthCard.jsx
+++ components/widgets/user/AuthCard/AuthCard.jsx
@@ -4,7 +4,7 @@ import css from './AuthCard.module.css';
 export const AuthCard = () => {
     return (
         <HBoxPanel className={`${css.card}`}>
-
+            
         </HBoxPanel>
     );
 };
