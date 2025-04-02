b5606426395bd30c8760fc9195e3f5691a85a752
Никита Фомин <defdXs@yandex.ru>
2025-04-02 16:47:04 +0300

    Added avatar to sidebar

--- components/widgets/ProfileSidebar/ProfileSidebar.jsx
+++ components/widgets/ProfileSidebar/ProfileSidebar.jsx
@@ -10,6 +10,7 @@ import { useAuth } from '../../../hooks/useAuth';
 import ExButton from '../../ui/buttons/ExButton/ExButton';
 import { useTheme } from '../../../hooks/useTheme';
 import { Close, Logout, Settings, Swap, User, UserAdd } from '../../dummies/Icons';
+import { Avatar } from '../../pages/base/Avatar';
 
 export const ProfileSidebar = ({ ref, className, active, onMinimized }) => {
     const navigate = useNavigate();
@@ -19,7 +20,7 @@ export const ProfileSidebar = ({ ref, className, active, onMinimized }) => {
         logout(true);
         navigate('/user/auth');
     };
-    
+
     const handleAccountSwitch = (e) => {
         if (e === accounts.length) {
             navigate('/user/auth');
@@ -30,60 +31,61 @@ export const ProfileSidebar = ({ ref, className, active, onMinimized }) => {
 
     return (
         <Sidebar className={`${css.profileSidebar} ${className} ${css.profileSidebar}`}
-                 ref={ref}
-                 side={1}
-                 mode={1}
-                 active={active}
-                 onMinimized={onMinimized}
-                 fullSize='var(--user-sidebar-width)'>
+            ref={ref}
+            side={1}
+            mode={1}
+            active={active}
+            onMinimized={onMinimized}
+            fullSize='var(--user-sidebar-width)'>
             <VBoxPanel gap='10px'
-                       padding='10px'>
+                padding='10px'>
                 <HBoxPanel gap='5px'
-                           valign='center'>
-                    <User className={`icon-l round pad5`} style={{
-                          border: '2px solid var(--font-color)',
-                          marginRight: '5px'
-                    }}/>
+                    valign='center'>
+                    {user.avatarId ? <Avatar className={'icon-g r100'}/> :
+                        <User className={`icon-l round pad5`} style={{
+                            border: '2px solid var(--font-color)',
+                            marginRight: '5px'
+                        }} />}
                     <VBoxPanel gap='5px'>
                         <span className='title'>{auth?.login}</span>
                         <span className={`${css.name} caption`}>{user?.firstname} {user?.lastname}</span>
                     </VBoxPanel>
                     <Dropout className={`${css.last}`}
-                             side={0}
-                             onSelect={handleAccountSwitch}
-                             options={[
-                                ...(accounts.filter(a => a.login !== auth?.login).map((a, i) => 
-                                    <SmallUserItem avatar={a.avatar}
-                                                   username={a.login}
-                                                //    name={`${a.firstname} ${a.lastname}`}
-                                                   key={i}/>
-                                )),
-                                <HBoxPanel className={css.addAccountButton}
-                                           gap='10px'
-                                           valign='center'>
-                                    <UserAdd className='icon-m'/>
-                                    <span>Добавить</span>
-                                </HBoxPanel>
-                             ]}>
-                        <Swap className={`icon-s textbutton pad5 r5`}/>
+                        side={0}
+                        onSelect={handleAccountSwitch}
+                        options={[
+                            ...(accounts.filter(a => a.login !== auth?.login).map((a, i) =>
+                                <SmallUserItem avatar={a.avatar}
+                                    username={a.login}
+                                    //    name={`${a.firstname} ${a.lastname}`}
+                                    key={i} />
+                            )),
+                            <HBoxPanel className={css.addAccountButton}
+                                gap='10px'
+                                valign='center'>
+                                <UserAdd className='icon-m' />
+                                <span>Добавить</span>
+                            </HBoxPanel>
+                        ]}>
+                        <Swap className={`icon-s textbutton pad5 r5`} />
                     </Dropout>
-                    <Close className={`icon-s textbutton pad5 r5`} onClick={onMinimized}/>
+                    <Close className={`icon-s textbutton pad5 r5`} onClick={onMinimized} />
                 </HBoxPanel>
 
-                <HDivider/>
+                <HDivider />
 
                 <ExButton className={`textbutton`}
-                          hAlign='start'
-                          onClick={() => navigate('/profile/settings')}
-                          gap='10px'><Settings className='icon-m'/> Настройки</ExButton>
+                    hAlign='start'
+                    onClick={() => navigate('/profile/settings')}
+                    gap='10px'><Settings className='icon-m' /> Настройки</ExButton>
 
-                <HDivider/>
+                <HDivider />
 
                 <ExButton iconClassName='rect25'
-                          className={`textbutton`}
-                          hAlign='start'
-                          onClick={handleLogout}
-                          gap='10px'><Logout className='icon-m'/> Выйти</ExButton>
+                    className={`textbutton`}
+                    hAlign='start'
+                    onClick={handleLogout}
+                    gap='10px'><Logout className='icon-m' /> Выйти</ExButton>
             </VBoxPanel>
         </Sidebar>
     );
