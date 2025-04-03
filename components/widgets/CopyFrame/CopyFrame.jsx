179a06539790d38f420d52430628afb6bd4dfe5a
Никита Фомин <defdXs@yandex.ru>
2025-04-03 17:39:45 +0300

    use new hook

--- components/widgets/CopyFrame/CopyFrame.jsx
+++ components/widgets/CopyFrame/CopyFrame.jsx
@@ -1,19 +1,18 @@
 import toast from 'react-hot-toast';
 import css from './CopyFrame.module.css';
+import { useClipboard } from '../../../hooks/useClipboard';
 
 export const CopyFrame = ({ ref, className, src, children }) => {
+    const { copy } = useClipboard();
+
     const handleCopy = () => {
-        if (src && src != "") {
-            navigator.clipboard.writeText(src).then(function() {
-                toast.success('Скопировано!');
-            }).catch(function(error) {
-                toast.error('Ошибка при копировании');
-            });
-        } else {
-            toast.error('Нечего копировать');
-        }
+        copy(src,
+            () => toast.success('Скопировано!'),
+            () => toast.error('Ошибка при копировании'),
+            () => toast.error('Нечего копировать')
+        );
     };
-    
+
     return (
         <div ref={ref} className={`${css.cf} ${className} pad5 r10 flex row center pointer`} onClick={handleCopy}>
             {children}
