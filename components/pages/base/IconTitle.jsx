da460addb920eb42332606cd72d5fb79ece0a0ad
Nikita Fomin <defdXs@yandex.ru>
2025-04-04 01:11:30 +0300

    now title redirects to home

--- components/pages/base/IconTitle.jsx
+++ components/pages/base/IconTitle.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { Pizza } from '../../dummies/Icons';
 import css from './base.module.css';
 
@@ -6,8 +7,10 @@ export const IconTitle = ({
     text = 'ENVELOPE',
     icon = <Pizza/>
 }) => {
+    const navigate = useNavigate();
+
     return (
-        <div className={`${className} flex row g20 center ${css.title}`}>
+        <div className={`${className} flex row g20 center pointer ${css.title}`} onClick={() => navigate('/')}>
             <span style={{margin: '0'}} className='h1'>{text}</span>
             <div className={`icon-l ${css.h960}`}>{icon}</div>
         </div>
