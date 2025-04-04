3e6ea1d4ee0650b71d77acfb31191ce371bfc5f7
Nikita Fomin <defdXs@yandex.ru>
2025-04-05 01:50:17 +0300

    Fixed avatar object fit

--- components/pages/base/Avatar.jsx
+++ components/pages/base/Avatar.jsx
@@ -1,6 +1,7 @@
 import { url } from "../../../../api/image";
 import { useAuth } from "../../../hooks/useAuth";
 import { User } from "../../dummies/Icons";
+import css from './base.module.css';
 
 export const Avatar = ({
     className,
@@ -9,7 +10,7 @@ export const Avatar = ({
     const { user } = useAuth();
 
     return (
-        user?.avatarId ? <img className={`${className}`} src={url(user.avatarId)} alt="Avatar" {...props} /> : 
+        user?.avatarId ? <img className={`${className} ${css.avatar}`} src={url(user.avatarId)} alt="Avatar" {...props} /> : 
         <User className={`${className}`} {...props} />
     );
 };
